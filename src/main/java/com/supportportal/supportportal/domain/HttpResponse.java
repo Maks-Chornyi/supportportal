f62dd03ca82d6b76f5044b0fbe4a0c36aa71264e
Maks Chornyi <maksym.chornyi@oa.edu.ua>
2021-12-13 22:23:29 +0200

    added httpResponse as general response for requests (#2)
    Co-authored-by: Maks-Chornyi <--replace-all>

---
+++ src/main/java/com/supportportal/supportportal/domain/HttpResponse.java
@@ -0,0 +1,12 @@
+package com.supportportal.supportportal.domain;
+
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+@Data
+public class HttpResponse {
+    private int httpStatusCode;
+    private HttpStatus httpStatus;
+    private String reason;
+    private String message;
+}
