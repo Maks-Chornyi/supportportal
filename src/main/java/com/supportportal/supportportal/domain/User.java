9ff3491a27171160376b5d896edaf1bc4d678ae9
Maks Chornyi <maksym.chornyi@oa.edu.ua>
2021-12-13 22:19:32 +0200

    SP-1: added user entity (#1)
    Co-authored-by: Maks-Chornyi <--replace-all>

---
+++ src/main/java/com/supportportal/supportportal/domain/User.java
@@ -0,0 +1,38 @@
+package com.supportportal.supportportal.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    private String userId;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private String email;
+    private String profileImgUrl;
+    private Date lastLoginDate;
+    private Date lastLoginDateDisplay;
+    private Date joinDate;
+    private String[] roles;
+    private String[] authorities;
+    private boolean isActive;
+    private boolean isNotLocked;
+
+}
