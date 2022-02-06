fec8c1369f8be8c868f258b24c4081aac197bff6
Maks Chornyi <maksym.chornyi@oa.edu.ua>
2022-02-06 19:45:44 +0200

    SP-6: implemented security with jwt (#8)
    Co-authored-by: Maks-Chornyi <--replace-all>

---
+++ src/main/java/com/supportportal/supportportal/config/SecurityConfigs.java
@@ -0,0 +1,68 @@
+package com.supportportal.supportportal.config;
+
+import com.supportportal.supportportal.config.constant.SecurityConstant;
+import com.supportportal.supportportal.filter.JWTAuthenticationEntryPoint;
+import com.supportportal.supportportal.filter.JWTAuthorizationFilter;
+import com.supportportal.supportportal.filter.JwtAccessDeniedHandler;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)//allows to have security on method level
+@RequiredArgsConstructor
+public class SecurityConfigs extends WebSecurityConfigurerAdapter {
+
+    private final JWTAuthorizationFilter jwtAuthorizationFilter;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final JWTAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+    @Qualifier("userDetailsService")
+    @NonNull
+    private final UserDetailsService userDetailsService;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder);
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable().cors()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers(SecurityConstant.PUBLIC_URLS).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .exceptionHandling().accessDeniedHandler(jwtAccessDeniedHandler)
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .and()
+                .addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManagerBean();
+    }
+}
--- src/main/java/com/supportportal/supportportal/domain/HttpResponse.java
+++ src/main/java/com/supportportal/supportportal/domain/HttpResponse.java
@@ -1,12 +1,18 @@
 package com.supportportal.supportportal.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import lombok.NonNull;
 import org.springframework.http.HttpStatus;
+
+import java.util.Date;
 
 @Data
 public class HttpResponse {
-    private int httpStatusCode;
-    private HttpStatus httpStatus;
-    private String reason;
-    private String message;
+    @NonNull private final int httpStatusCode;
+    @NonNull private final HttpStatus httpStatus;
+    @NonNull private final String reason;
+    @NonNull private final String message;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM-dd-yyyy hh:mm:ss", timezone = "Africa/Cairo")
+    private Date timeStamp = new Date();
 }
