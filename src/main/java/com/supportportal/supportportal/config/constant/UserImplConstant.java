8a6e9e374c274ccc988e0abe5a92b19a0bdb403d
Maks-Chornyi <--replace-all>
2022-02-07 21:21:15 +0200

    SP-8: privided possibility to register in app

---
+++ src/main/java/com/supportportal/supportportal/config/constant/UserImplConstant.java
@@ -0,0 +1,7 @@
+package com.supportportal.supportportal.config.constant;
+
+public class UserImplConstant {
+    public static final String USERNAME_ALREADY_EXIST = "Username already exist: ";
+    public static final String EMAIL_ALREADY_EXIST = "Email already exist: ";
+    public static final String NO_USER_FOUND_WITH_USERNAME = "No user found with username: ";
+}
--- src/main/java/com/supportportal/supportportal/service/impl/UserServiceImpl.java
+++ src/main/java/com/supportportal/supportportal/service/impl/UserServiceImpl.java
@@ -2,19 +2,32 @@ package com.supportportal.supportportal.service.impl;
 
 import com.supportportal.supportportal.domain.User;
 import com.supportportal.supportportal.domain.UserPrincipal;
+import com.supportportal.supportportal.exception.domain.EmailExistException;
+import com.supportportal.supportportal.exception.domain.UserNotFoundException;
+import com.supportportal.supportportal.exception.domain.UsernameExistException;
 import com.supportportal.supportportal.repository.UserRepository;
 import com.supportportal.supportportal.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.transaction.Transactional;
 import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import static com.supportportal.supportportal.config.constant.Role.ROLE_HR;
+import static com.supportportal.supportportal.config.constant.Role.ROLE_USER;
+import static com.supportportal.supportportal.config.constant.UserImplConstant.*;
 
 @Service
 @Transactional
@@ -24,11 +37,12 @@ public class UserServiceImpl implements UserService, UserDetailsService {
 
     private final UserRepository userRepository;
     private final Logger LOGGER = LoggerFactory.getLogger(getClass());
+    private final BCryptPasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findUserByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+                .orElseThrow(() -> new UsernameNotFoundException(NO_USER_FOUND_WITH_USERNAME + username));
 
         user.setLastLoginDateDisplay(user.getLastLoginDate());
         user.setLastLoginDate(new Date());
@@ -37,5 +51,83 @@ public class UserServiceImpl implements UserService, UserDetailsService {
         LOGGER.info("Returning found user: " + username);
 
         return userPrincipal;
+    }
+
+    @Override
+    public User register(User user) {
+        validateNewUsernameAndEmail(StringUtils.EMPTY, user.getUsername(), user.getEmail());
+        user.setUserId(generateUserId());
+        String password = generatePassword();
+        String encodedPassword = encodePassword(password);
+        user.setJoinDate(new Date());
+        user.setPassword(encodedPassword);
+        user.setActive(true);
+        user.setNotLocked(true);
+        user.setRole(ROLE_USER.name());
+        user.setAuthorities(ROLE_HR.getAuthorities());
+        user.setProfileImgUrl(getTemporaryProfileImageUrl());
+        userRepository.save(user);
+        LOGGER.info("New user password: " + password);
+
+        return user;
+    }
+
+    private String getTemporaryProfileImageUrl() {
+        return ServletUriComponentsBuilder.fromCurrentContextPath().path("/user/image/profile/temp").toUriString();
+    }
+
+    private String encodePassword(String password) {
+        return passwordEncoder.encode(password);
+    }
+
+    private String generatePassword() {
+        return RandomStringUtils.randomAlphanumeric(10);
+    }
+
+    private String generateUserId() {
+        return RandomStringUtils.randomNumeric(10);
+    }
+
+    private User validateNewUsernameAndEmail(String currentUserName, String newUserName, String newEmail) {
+        if (StringUtils.isNotBlank(currentUserName)) {
+            final User currentUser = userRepository.findUserByUsername(currentUserName)
+                    .orElseThrow(() -> new UserNotFoundException(NO_USER_FOUND_WITH_USERNAME + currentUserName));
+
+            final Optional<User> userByUsername = userRepository.findUserByUsername(newUserName);
+            userByUsername.ifPresent(user -> {
+                throw new UsernameExistException(USERNAME_ALREADY_EXIST + newUserName);
+            });
+
+            final Optional<User> userByEmail = userRepository.findUserByEmail(newEmail);
+            userByEmail.ifPresent(user -> {
+                throw new EmailExistException(EMAIL_ALREADY_EXIST + newEmail);
+            });
+            return currentUser;
+        } else {
+            userRepository.findUserByUsername(newUserName).
+                    ifPresent(usr -> {
+                        throw new UsernameExistException(USERNAME_ALREADY_EXIST + newUserName);
+                    });
+            userRepository.findUserByEmail(newEmail)
+                    .ifPresent(user -> {
+                        throw new EmailExistException(EMAIL_ALREADY_EXIST + newEmail);
+                    });
+            return null;
+        }
+    }
+
+    @Override
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User findUserByUserName(String username) {
+        return null;
+    }
+
+    @Override
+    public User findUserByEmail(String email) {
+        return null;
     }
 }
