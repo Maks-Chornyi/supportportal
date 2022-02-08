6ccafea7bd5686bae06e44c8be2bae0898b9c69a
Maks Chornyi <maksym.chornyi@oa.edu.ua>
2022-02-08 20:42:23 +0200

    SP-8: added brut force protection (#12)
    Co-authored-by: Maks-Chornyi <--replace-all>

---
+++ src/main/java/com/supportportal/supportportal/listener/AuthenticationFailureListener.java
@@ -0,0 +1,24 @@
+package com.supportportal.supportportal.listener;
+
+import com.supportportal.supportportal.service.LoginAttemptService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@RequiredArgsConstructor
+public class AuthenticationFailureListener {
+
+    private final LoginAttemptService loginAttemptService;
+
+    @EventListener
+    public void onAuthenticationFailure(AuthenticationFailureBadCredentialsEvent event) {
+        Object principal = event.getAuthentication().getPrincipal();
+        if (principal instanceof String) {
+            String username = (String) principal;
+            loginAttemptService.addUserToLoginAttemptCache(username);
+        }
+    }
+}
---
+++ src/main/java/com/supportportal/supportportal/listener/AuthenticationSuccessListener.java
@@ -0,0 +1,25 @@
+package com.supportportal.supportportal.listener;
+
+import com.supportportal.supportportal.domain.User;
+import com.supportportal.supportportal.domain.UserPrincipal;
+import com.supportportal.supportportal.service.LoginAttemptService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AuthenticationSuccessListener {
+
+    private final LoginAttemptService loginAttemptService;
+
+    @EventListener
+    public void onAuthenticationSuccess(AuthenticationSuccessEvent event) {
+        Object principal = event.getAuthentication().getPrincipal();
+        if (principal instanceof UserPrincipal) {
+            UserPrincipal user = (UserPrincipal) principal;
+            loginAttemptService.evictUserFromLoginAttemptCache(user.getUsername());
+        }
+    }
+}
